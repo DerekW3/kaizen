2621df1ec02c196435c0731a605db5e757444a39
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-10-06 20:45:37 -0500

    Feat: Start implementation of cursor moving

--- src/editor/cursor.rs
+++ src/editor/cursor.rs
@@ -1,4 +1,6 @@
 use crate::editor::terminal::{Shape, Terminal};
+use core::cmp::min;
+use std::io::Error;
 
 pub struct Cursor {
     x: u16,
@@ -16,5 +18,21 @@ impl Cursor {
 
     pub fn row(&self) -> u16 {
         self.y
+    }
+
+    pub fn move_up(&mut self) -> Result<(), Error> {
+        self.y = self.y.saturating_sub(1);
+        Terminal::move_cursor(self.x, self.y)?;
+        Ok(())
+    }
+
+    pub fn move_down(&mut self) -> Result<(), Error> {
+        let Shape { height, .. } = Terminal::shape()?;
+        self.y = min(
+            self.y.saturating_add(1),
+            u16::try_from(height).unwrap_or(u16::MAX),
+        );
+        Terminal::move_cursor(self.x, self.y)?;
+        Ok(())
     }
 }
